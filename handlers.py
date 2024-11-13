dbe17ba496cb4e7b18c87365c6984dc1aa0669cf
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-11-13 23:50:12 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 from telethon import TelegramClient
 from telethon.sessions import StringSession as TelethonStringSession
-from telethon.tl.functions.messages import ExportChatInviteRequestfrom p
+from telethon.tl.functions.messages import ExportChatInviteRequestfrom 
 from pyrogram import Client as PyrogramClient, StringSession as PyrogramStringSession
 from pyrogram.types import Message
 from kvsqlite.sync import Client as DataClient
